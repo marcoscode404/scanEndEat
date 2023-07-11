a4f75f5608b37c02ad15e9d90d4ae68e00b5f749
marcos gomes <marcos.gomes@modenaesilva.com.br>
2023-07-11 13:55:43 -0400

    feat: criando pagina Home

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,53 @@
 <script setup lang="tsx">
-
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 </script>
 
 <template>
     <div class="heightCalcManager">
-        <div class="container m-auto mt-2">
+        <div class="container m-auto mt-2 ">
             <h1 class="text-center">
                 Data:22/06/23 as 14:25
             </h1>
+
+            <div class=" w-52 mt-40 h-40 m-auto">
+                <img src="../../assets/imgQrCode.svg" class=" w-full h-full" />
+
+                <GlobalButton type="defaultSecondary"
+                    class="flex justify-center gap-1 bg-black text-white w-full py-2 mt-10">
+                    Camera
+                    <Icon name="jam:camera" class="text-2xl" />
+                </GlobalButton>
+            </div>
+
         </div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <TabGroup>
+        <TabList class="w-full bg-black text-white font-semibold flex justify-center gap-20
+        absolute
+        ">
+            <Tab>Home</Tab>
+            <Tab>Histórico</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel>Content 1</TabPanel>
+            <TabPanel>Content 2</TabPanel>
+        </TabPanels>
+    </TabGroup>
 </template>
 
 <style>
